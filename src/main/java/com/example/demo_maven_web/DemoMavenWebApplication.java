5f276af198bcf52876334263d841cd74cb949b11
mjz007 <janzma@qq.com>
2021-04-09 14:04:14 +0800

    测试提交

---
+++ src/main/java/com/example/demo_maven_web/DemoMavenWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_maven_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMavenWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoMavenWebApplication.class, args);
+    }
+
+}
